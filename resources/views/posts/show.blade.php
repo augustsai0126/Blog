34457a40461c46bf296d2bb3dd65bc225ddb05aa
DESKTOP-QCDVH7A\x4017 <x820126@gmail.com>
2016-11-20 22:10:07 +0800

    1120 在顯示頁面 顯示時間跟修改及刪除按鈕

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -4,7 +4,39 @@
 
 @section('content')
 
-    <h1>{{ $post->title }}</h1>
-    <p class="lead">{{ $post->body }}</p>
+    <div class="row">
+      <div class="col-md-8">
+          <h1>{{ $post->title }}</h1>
+          <p class="lead">{{ $post->body }}</p>
+      </div>
+
+      <div class="col-md-4">
+        <div class="well">
+          <dl class="dl-horizontal">
+            <dt>日期:</dt>
+            <dd>{{ $post->created_at }}</dd>
+          </dl>
+
+          <dl class="dl-horizontal">
+            <dt>上次更新</dt>
+            <dd>{{ $post->updated_at }}</dd>
+          </dl>
+          <hr>
+          <div class="row">
+            <div class="col-sm-6">
+              {!! Html::linkRoute('posts.edit', '編輯', array($post->id), array('class' => 'btn btn-primary btn-block')) !!}
+              <!--
+              用上面laravel的用法 取代原先的a連結
+              <a href="#" class="btn btn-primary btn-block">編輯</a>
+              -->
+            </div>
+            <div class="col-sm-6">
+              {!! Html::linkRoute('posts.destroy', '刪除', array($post->id), array('class' => 'btn btn-danger btn-block')) !!}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
 
 @endsection
